e04ec0a8a6891d5f526c896a0ff89d5773d2339c
FiftyFiveAA <90160593+FiftyFiveAA@users.noreply.github.com>
2023-03-20 21:18:25 -0500

    Update evm.py

--- evm.py
+++ evm.py
@@ -1,8 +1,10 @@
 import api_server
+import web_server
 
 APIServer_thread = api_server.APIServer(12345)
 APIServer_thread.start()
 
-print("hi")
+WebServer_thread = web_server.WebServer(12346)
+WebServer_thread.start()
 
 
